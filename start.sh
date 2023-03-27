4de967ac8d9f69269064312200b099194133392a
psnprasann <106792309+psnprasann@users.noreply.github.com>
2023-03-27 13:25:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/psnprasann/LazyPrincess/tree/artificial_inteligence_v7.5 /LazyPrincess
+  git clone https://github.com/psnprasann/LazyPrincess /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
